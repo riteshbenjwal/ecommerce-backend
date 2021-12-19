fc10526145902bd124a108bfec5c714a9740c349
Ritesh Benjwal <riteshbenjwal7@gmail.com>
2021-12-19 20:02:40 +0530

    BIG_PROMISES

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,7 +1,73 @@
-exports.home = (req, res) => {
+const BigPromise = require("../middlewares/bigPromise");
+
+exports.home = BigPromise((req, res) => {
   res.status(200).json({
     success: true,
     greeting: "Hello from API",
   });
-};
+});
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*ALternative to BigPromise
+
+exports.home = (req, res) => {
+  try{
+  res.status(200).json({
+    success: true,
+    greeting: "Hello from API",
+  });
+
+}
+catch(err){
+  console.log(err);
+}
+}
+
+*/
---
+++ utils/customError.js
@@ -0,0 +1,17 @@
+class CustomError extends Error{
+
+    constructor(message, statusCode){
+        super(message)
+        this.code = statusCode
+    
+    
+    
+    }
+
+
+
+
+}
+
+
+module.exports = CustomError;
