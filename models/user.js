ab3b70ba51068bf3caff9cdcb02d898d0eeef29b
Ritesh Benjwal <riteshbenjwal7@gmail.com>
2021-12-20 15:18:07 +0530

    USER_MODEL_DESIGNED

---
+++ models/user.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please provide a name"],
+    maxlength: [50, "Name can not be more than 50 characters"],
+  },
+  email: {
+    type: String,
+    required: [true, "Please provide an email"],
+    validate: [validator.isEmail, "Please provide a valid email"],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Please provide a password"],
+    minlength: [6, "Password must be at least 6 characters"],
+    select: false, //This will not be returned when querying the user
+  },
+  role: {
+    type: String,
+    default: "user",
+  },
+  photo: {
+    id: {
+      type: String,
+      required: true,
+    },
+    secure_url: {
+      type: String,
+      required: true,
+    },
+  },
+  forgotPasswordToken: String,
+  forgotPasswordExpiry: Date,
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("User", userSchema);
