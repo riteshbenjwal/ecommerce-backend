720aa7e0d835dbc8df062c793d5d8a7c0d9d98a1
Ritesh Benjwal <riteshbenjwal7@gmail.com>
2021-12-22 20:14:31 +0530

    SIGN_IN_SIGN_OUT_ROUTE

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,20 +6,24 @@ const fileUpload = require("express-fileupload");
 const cloudinary = require("cloudinary");
 
 exports.signup = BigPromise(async (req, res, next) => {
-  let result;
-  if (req.files) {
-    let file = req.files.photo;
-    result = await cloudinary.v2.uploader.upload(file.tempFilePath, {
-      folder: "users",
-      width: 150,
-      crop: "scale",
-    });
+  // let result;
+
+  if (!req.files) {
+    return next(new CustomError("photo is required for signup", 400));
   }
+
   const { name, email, password } = req.body;
 
   if (!email || !name || !password) {
     return next(new CustomError("Name, email and Password are required", 400));
   }
+
+  let file = req.files.photo;
+  const result = await cloudinary.v2.uploader.upload(file.tempFilePath, {
+    folder: "users",
+    width: 150,
+    crop: "scale",
+  });
 
   const user = await User.create({
     name,
@@ -32,4 +36,53 @@ exports.signup = BigPromise(async (req, res, next) => {
   });
 
   cookieToken(user, res);
+});
+
+exports.login = BigPromise(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  //Check for presence of email and password
+
+  if (!email || !password) {
+    return next(new CustomError("Email and Password are required", 400));
+  }
+
+  //Get user from db
+
+  const user = await User.findOne({ email }).select("+password");
+
+  //If user not found in db
+
+  if (!user) {
+    return next(
+      new CustomError("Your are not registered in our database", 400)
+    );
+  }
+
+  //match the password
+
+  const isPasswordCorrect = await user.isValidatePassword(password);
+
+  //If password does not match
+
+  if (!isPasswordCorrect) {
+    return next(
+      new CustomError("Email or Password does not match or exist", 400)
+    );
+  }
+
+  //all goes good and send the token
+  cookieToken(user, res);
+});
+
+exports.logout = BigPromise(async (req, res, next) => {
+  res.cookie("token", null, {
+    expires: new Date(Date.now()),
+    httpOnly: true,
+  });
+
+  res.status(200).json({
+    success: true,
+    message: "Logout Successful",
+  });
 });
--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,11 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
 
-const {signup} = require('../controllers/userController');
- 
-router.route('/signup').post(signup);
+const { signup, login, logout } = require("../controllers/userController");
 
+router.route("/signup").post(signup);
+router.route("/login").post(login);
+router.route("/logout").get(logout);
 
 module.exports = router;
